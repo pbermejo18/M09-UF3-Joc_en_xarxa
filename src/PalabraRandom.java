81df5e5702cfeea5986bc30e72be2c360df4b92e
Pol <pbermejo@elpuig.xeill.net>
2022-02-24 17:35:01 +0100

    24-02-2022 1734

--- src/PalabraRandom.java
+++ src/PalabraRandom.java
@@ -1,7 +1,4 @@
 public class PalabraRandom {
-    /* Classe que genera numeros aleatoris per jugar a adivinar-los i els comprova
-     * tant si rep un numero o una string.
-     */
 
     private String paraula;
 
@@ -11,7 +8,6 @@ public class PalabraRandom {
     }
 
     public String pensa() {
-        // setNum((int) ((Math.random()*max)+1));
         String[] paraules = {"Menja", "Pizza", "Pinya", "Linux"};
 
         int numero = (int) (Math.random()*4);
@@ -25,13 +21,7 @@ public class PalabraRandom {
         if(paraula.equals(n)) return "0";
         else return "1";
     }
-/*
-    public String comprova(String s) {
-        int n = Integer.parseInt(s);
-        if(paraula.equals(s)) return "Correcte";
-        else return "Una altre paraula";
-    }
-*/
+
     public String getParaula() {
         return paraula;
     }
